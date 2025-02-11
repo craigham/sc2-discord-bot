88f7dde0a82e4672b175d1bd8100d5b84e25aa8a
Craig Hamilton <craigh@quailholdings.com>
2025-02-11 07:22:53 -0800

    can request match only specifying opponent, random map

--- run_match.py
+++ run_match.py
@@ -1,19 +1,88 @@
-
-import subprocess
+from collections import namedtuple
 from pathlib import Path
-from random import choice
+import random
 import json
 import discord
+import asyncio
+from dotenv import load_dotenv
+import os
 
-player1 = 'TBone'
+# Load environment variables from .env file
+load_dotenv()
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+PLAYER1 = os.getenv('PLAYER1')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+
 MAPS_PATH = './maps'
-LADDERBOTS_TYPE = {"BinaryCpp": "cpplinux", "Python":"python", "DotNetCore": "dotnetcore"}
+LADDERBOTS_TYPE = {"BinaryCpp": "cpplinux", "Python": "python", "DotNetCore": "dotnetcore"}
 MAP_FILE_EXT = 'SC2Map'
-NUM_GAMES_TO_PLAY = 1
-DISCORD_TOKEN = '<Token>'
+BOTS = sorted((x.name for x in Path('./bots').iterdir() if not x.is_file()))
+MAPS = sorted(file.name.rstrip('AIE.SC2Map') for file in Path('./maps').iterdir()
+                  if file.is_file()
+                  and file.name.split('.')[-1] == 'SC2Map'
+                  and not file.name.startswith('.'))
+
+SC2Match = namedtuple('SC2Match', ['map', 'bot1', 'bot2', 'priority'])
+
+def get_results_json():
+    with open('results.json', 'r') as results_file:
+        return json.load(results_file)['results']
+        
+def get_bot_exe_type(bot_name):
+    try:
+        with open(f'bots/{bot_name}/ladderbots.json', 'r') as results_file:
+            bot_info = json.load(results_file)
+            bot_type = bot_info['Bots'][bot_name]['Type']
+            return LADDERBOTS_TYPE[bot_type]
+    except FileNotFoundError:
+        return "python"
+
+class Sc2Runner(discord.Client):
+    def __init__(self, bot_name, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bot_name = bot_name
+        self.match_queue = []
+        self.queue_task = None
+        self.current_match = None
+
+    def queue_match(self, opponent, map_name):
+        self.match_queue.append(SC2Match(map_name, self.bot_name, opponent, 3))
+
+    async def process_queue(self):
+        while True:
+            if self.match_queue:
+                match = self.match_queue.pop(0)
+                self.current_match = match
+                await self.do_match(match)
+                print(f'Match ended: {match}')
+                await self.report_result(match)
+                self.current_match = None                
+                
+            await asyncio.sleep(3)  # Sleep to prevent tight loop
+
+    async def report_result(self, match:SC2Match):
+        match_results = f'Match results: {get_results_json()[-1]}'
+        print(match_results)
+        # await self.get_channel(CHANNEL_ID).send(match_results)
+
+    async def do_match(self, match:SC2Match):        
+        matchString = f"1,{match.bot1},T,python,2,{match.bot2},T,{get_bot_exe_type(match.bot2)},{match.map}"
+        with open("matches", "w") as f:
+            f.write(f"{matchString}")
+        command = f'docker-compose -f docker-compose-host-network.yml up'
+        process = await asyncio.create_subprocess_shell(command, shell=True, executable='/bin/bash')
+        await process.communicate()
+        
+    async def setup_hook(self):
+        self.queue_task = self.loop.create_task(self.process_queue())        
+        
+        
+# channel = await self.fetch_channel(CHANNEL_ID)
+# await channel.send(f'Bot {self.bot_name} is ready to queue matches')
+
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+client = Sc2Runner(PLAYER1, intents=intents)
 
 @client.event
 async def on_ready():
@@ -22,49 +91,23 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        return    
+        return
     if message.content.startswith('!match'):
-        _, opponent, the_map = message.content.split()
-        await message.channel.send(f'Starting match against: {opponent} on map: {the_map}')
-        await do_match(opponent, the_map + 'AIE')                
-        await message.channel.send(f'{get_game_result()}')
-    elif message.content.startswith('!bots'): 
-        await message.channel.send(f'{get_bot_list()}')
-    elif message.content.startswith('!maps'):        
-        await message.channel.send(f'{get_map_list()}')
-        
-def get_game_result():
-    with open('results.json' , 'r') as results_file:
-        game_results = json.load(results_file)        
-        return game_results['results'][-1]
-        
-def get_bot_list():
-    return sorted((x.name for x in Path('./bots').iterdir() if not x.is_file()))
-    
-def get_map_list():    
-    return sorted(file.name.rstrip('AIE.SC2Map') for file in Path('./maps').iterdir() 
-                                    if file.is_file() 
-                                    and file.name.split('.')[-1] == 'SC2Map'
-                                    and not file.name.startswith('.')
-            )
-
-async def do_match(opponent, map_name):    
-    matchString: str = f"1,{player1},T,python,2,{opponent},T,{get_bot_exe_type(opponent)},{map_name}"
-    with open("matches", "w") as f:
-        f.write(f"{matchString}")
-
-    command = f'docker-compose -f docker-compose-host-network.yml up'
-    subprocess.run(command, shell=True, executable='/bin/bash')
-        
-def get_bot_exe_type(bot_name):
-    try:
-        with open(f'bots/{bot_name}/ladderbots.json' , 'r') as results_file:
-            bot_info = json.load(results_file)
-            bot_type = bot_info['Bots'][bot_name]['Type']
-            return LADDERBOTS_TYPE[bot_type]
-    except FileNotFoundError:
-        return "python"
+        _, match_params = message.content.split()
+        if len(match_params.split()) == 2:
+            opponent, the_map = match_params.split()
+        else:
+            opponent = match_params        
+            the_map = random.choice(MAPS)
+        await message.channel.send(f'Queueing match against: {opponent} on map: {the_map}')
+        client.queue_match(opponent, the_map + 'AIE')
+    elif message.content.startswith('!bots'):
+        await message.channel.send(f'{BOTS}')
+    elif message.content.startswith('!maps'):
+        await message.channel.send(f'{MAPS}')
+    elif message.content.startswith('!queue'):
+        await message.channel.send(f'Current: {client.current_match} - Queue: {client.match_queue}')
+    elif message.content.startswith('!last_match'):
+        await message.channel.send(f'{get_results_json()[-1]}')
 
 client.run(DISCORD_TOKEN)
-
-
